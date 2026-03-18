3431d0fd9391b6feff566d5ca21e9e895f9eedf3
Wahidullah Karimi <wahidullah.karimi22@gmail.com>
2026-03-18 15:20:38 +0430

    JOBS, TAGS, TDD,

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,15 +1,15 @@
-@props(["size" => "base"])
+@props(["tag", "size" => "base"])
 
 @php
 $classes = "bg-white/10 rounded-xl text-xs font-bold hover:bg-white/25 transition-colors duration-300";
 
 if ($size === "base") {
-$classes .= " px-5 py-1 text-sm";
+$classes .= " px-4 py-2 text-sm";
 }
 
 if ($size === "small") {
-$classes .= " px-3 py-1 text-2xs";
+$classes .= " px-3 py-2 text-2xs";
 }
 @endphp
 
-<a href="#" class="{{ $classes }}">{{ $slot }}</a>
+<a href="/tags/{{ strtolower($tag->name) }}" class="{{ $classes }}">{{ $tag->name }}</a>
